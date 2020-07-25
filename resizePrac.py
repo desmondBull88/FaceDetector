0516cf831ef4f4f8d6f0569f0350d0e20fdea991
desmondBull88 <66146449+desmondBull88@users.noreply.github.com>
2020-07-24 21:27:41 -0700

    Final Changes

--- resizePrac.py
+++ resizePrac.py
@@ -1,13 +1,16 @@
 import cv2
+import pandas
 import time
-
+from datetime import datetime
 first_frame = None
-
+list_stat = [None, None]
+times = []
 vid = cv2.VideoCapture(0)
-
+df = pandas.DataFrame(columns=["Start", "End"])
 while True:
 
     check, frame = vid.read()
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -27,14 +30,29 @@ while True:
     for countour in cnts:
         if cv2.contourArea(countour) < 10000:
             continue
+        status = 1
+
         (x, y, w, h) = cv2.boundingRect(countour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
+    list_stat.append(status)
+
+    if list_stat[-1] == 1 and list_stat[-2] == 0:
+        times.append(datetime.now())
+    if list_stat[-1] == 0 and list_stat[-2] == 1:
+        times.append(datetime.now())
 
     cv2.imshow("Capturing", frame)
 
     key = cv2.waitKey(1)
 
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
+
+for i in range(0, len(times), 2):
+    df = df.append({"Start": times[i], "End": times[i+1]}, ignore_index=True)
+
+df.to_csv("Times.csv")
 vid.release()
 cv2.destroyAllWindows
