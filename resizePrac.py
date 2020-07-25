6750fda60e320873549ff8a3592a3fd6b99241cd
desmondBull88 <66146449+desmondBull88@users.noreply.github.com>
2020-07-24 19:29:59 -0700

    Update resizePrac.py

--- resizePrac.py
+++ resizePrac.py
@@ -25,7 +25,7 @@ while True:
                                  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for countour in cnts:
-        if cv2.contourArea(countour) < 1000:
+        if cv2.contourArea(countour) < 10000:
             continue
         (x, y, w, h) = cv2.boundingRect(countour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
